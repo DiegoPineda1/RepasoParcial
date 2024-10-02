1ee5b35379fe735a7a7701b73c810e6a4dcf8f09
Diego Pienda <412672@tecnicatura.frc.utn.edu.ar>
2024-10-02 15:50:11 -0300

    Repaso Parcial para 5/10

---
+++ RepasoParcial/CineApi/Controllers/PeliculaControlle.cs
@@ -0,0 +1,60 @@
+﻿using BackApi.Models;
+using BackApi.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CineApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculaControlle : ControllerBase
+    {
+        private readonly IPeliculaRepositorio _peliculaRepositorio;
+        public PeliculaControlle(IPeliculaRepositorio peliculaRepositorio)
+        {
+            _peliculaRepositorio = peliculaRepositorio;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_peliculaRepositorio.ObtenerTodas());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_peliculaRepositorio.ObtenerPorId(id));
+        }
+        [HttpPost]
+        public IActionResult Post(Pelicula pelicula)
+        {
+            pelicula.estreno = true;
+            if (string.IsNullOrEmpty(pelicula.titulo) || string.IsNullOrEmpty(pelicula.director)
+                || pelicula.anio == 0 || pelicula.id_genero == 0)
+            {
+                return BadRequest("Faltan datos");
+            }
+            if (_peliculaRepositorio.Guardar(pelicula))
+            {
+                return Ok("Pelicula guardada");
+            }
+            return BadRequest("Error al guardar la pelicula");
+        }
+        [HttpPut("Modificar Estado")]
+        public IActionResult Put(int id)
+        {
+            var Pelicula = _peliculaRepositorio.ObtenerPorId(id);
+            if(Pelicula == null)
+            {
+                return BadRequest("Pelicula no encontrada");
+            }
+            Pelicula.estreno = false;
+            if (_peliculaRepositorio.Modificar(id))
+            {
+                return Ok("Estado modificado");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+
+        }
+    }
+}
---
+++ RepasoParcial/RepasoParcial/Repositorio/PeliculaRepositorio.cs
@@ -0,0 +1,78 @@
+﻿using BackApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackApi.Repositorio
+{
+    public interface IPeliculaRepositorio
+    {
+        bool Guardar(Pelicula pelicula);
+        bool Modificar(int id);
+        bool Eliminar(int id);
+        List<Pelicula> ObtenerTodas();
+        Pelicula ObtenerPorId(int id);
+
+    }
+    public class PeliculaRepositorio : IPeliculaRepositorio
+    {
+        private readonly db_cineContext _context;
+        public PeliculaRepositorio(db_cineContext db_CineContext)
+        {
+            _context = db_CineContext;
+        }
+
+        public bool Eliminar(int id)
+        {
+            if(_context.Peliculas.Find(id) == null)
+            {
+                return false;
+            }
+            _context.Peliculas.Remove(_context.Peliculas.Find(id));
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Guardar(Pelicula pelicula)
+        {
+            if(pelicula == null)
+            {
+                return false;
+            }
+            _context.Peliculas.Add(pelicula);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Modificar(int id)
+        {
+            if(id == 0)
+            {
+                return false;
+            }
+            _context.Peliculas.Update(_context.Peliculas.Find(id));
+            _context.SaveChanges();
+            return true;
+        }
+
+        public Pelicula ObtenerPorId(int id)
+        {
+            if(_context.Peliculas.Find(id) == null)
+            {
+                return null;
+            }
+            return _context.Peliculas.Find(id);
+        }
+
+        public List<Pelicula> ObtenerTodas()
+        {
+            if(_context.Peliculas.Count() == 0)
+            {
+                return null;
+            }
+            return _context.Peliculas.Where(x => x.estreno == true).ToList();
+        }
+    }
+}
